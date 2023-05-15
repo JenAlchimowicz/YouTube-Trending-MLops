b481bf69a75e632fd17168fcf262a122c5d55b40
Jen Alchimowicz <jedrzejalchimowicz@gmail.com>
2023-05-15 21:51:36 +0200

    Add uploading raw data to s3

--- scripts/data_ingestion/data_download.sh
+++ scripts/data_ingestion/data_download.sh
@@ -4,3 +4,6 @@ kaggle datasets download -d "rsrishav/youtube-trending-video-dataset" -f "GB_you
 unzip -o -d data/raw_data data/raw_data/GB_youtube_trending_data.csv.zip
 rm data/raw_data/GB_youtube_trending_data.csv.zip
 kaggle datasets download -d "rsrishav/youtube-trending-video-dataset" -f "GB_category_id.json" -p "data/raw_data"
+
+aws s3 cp ./data/raw_data/GB_youtube_trending_data.csv s3://yt-trending-mlops/data/raw_data/ --profile yt-trending
+aws s3 cp ./data/raw_data/GB_category_id.json s3://yt-trending-mlops/data/raw_data/ --profile yt-trending
